634f42e9de586fee3a5353b2f14f62e3a15ec507
ChinChuanKuo <chinchuankuo@ChinChuanKuos-MacBook-Pro.local>
2021-03-01 21:38:13 +0800

    module better

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
 const
     express = require('express'),
     bodyParser = require('body-parser'),
-    router = express.Router(),
     path = require('path'),
     app = express().use(bodyParser.json()); // creates express http server
 
@@ -27,6 +26,12 @@ app.post('/webhook', (req, res) => {
 
             let sender_psid = webhook_event.sender.id;
             console.log('Sender PSID: ' + sender_psid);
+
+            if (webhook_event.message) {
+                handleMessage(sender_psid, webhook_event.message);
+            } else if (webhook_event.postback) {
+                handlePostback(sender_psid, webhook_event.postback);
+            }
         });
         // Returns a '200 OK' response to all requests
         res.status(200).send('EVENT_RECEIVED');
@@ -36,10 +41,54 @@ app.post('/webhook', (req, res) => {
     }
 });
 
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+    let response;
+    // Check if the message contains text
+    if (received_message.text) {
+        // Create the payload for a basic text message
+        response = {
+            "text": `You sent the message: "${received_message.text}". Now send me an image!`
+        }
+    }
+    // Sends the response message
+    callSendAPI(sender_psid, response);
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+    // Construct the message body
+    let request_body = {
+        "recipient": {
+            "id": sender_psid
+        },
+        "message": response
+    }
+    // Send the HTTP request to the Messenger Platform
+    let PAGE_ACCESS_TOKEN = "EAADICpHpt48BAFjdSPqey5xvFf3F6ceOYpPJSoImrslXdfGolIA9LzcMydCv9ZCPl8ZBW0i5uR1pJxZB7PqIQZAjIvSq6AteASgZARSkCUBVZBPuRWlgm0QbuSUR5g42xcbq77qNlY0GlZBBaPiOFjzgUC1RpJpac6tfCUouOwo7SNnT9IZAWr2RUieXYqNGZClQZD";
+    request({
+        "uri": "https://graph.facebook.com/v2.6/me/messages",
+        "qs": { "access_token": PAGE_ACCESS_TOKEN },
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+            console.log('message sent!')
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    });
+}
+
 // Adds support for GET requests to our webhook
 app.get('/webhook', (req, res) => {
     // Your verify token. Should be a random string.
-    let VERIFY_TOKEN = "EAADICpHpt48BAFjdSPqey5xvFf3F6ceOYpPJSoImrslXdfGolIA9LzcMydCv9ZCPl8ZBW0i5uR1pJxZB7PqIQZAjIvSq6AteASgZARSkCUBVZBPuRWlgm0QbuSUR5g42xcbq77qNlY0GlZBBaPiOFjzgUC1RpJpac6tfCUouOwo7SNnT9IZAWr2RUieXYqNGZClQZD"
+    let VERIFY_TOKEN = "EAADICpHpt48BAFjdSPqey5xvFf3F6ceOYpPJSoImrslXdfGolIA9LzcMydCv9ZCPl8ZBW0i5uR1pJxZB7PqIQZAjIvSq6AteASgZARSkCUBVZBPuRWlgm0QbuSUR5g42xcbq77qNlY0GlZBBaPiOFjzgUC1RpJpac6tfCUouOwo7SNnT9IZAWr2RUieXYqNGZClQZD";
     // Parse the query params
     let mode = req.query['hub.mode'];
     let token = req.query['hub.verify_token'];
