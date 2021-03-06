66b5b223f10e2369daa7c754002107d95a3937f9
ChinChuanKuo <chinchuankuo@ChinChuanKuos-MacBook-Pro.local>
2021-03-06 11:08:11 +0800

    module better

--- services/profileService.js
+++ services/profileService.js
@@ -44,7 +44,7 @@ let handleSetupProfileAPI = () => {
                 "json": request_body
             }, (err, res, body) => {
                 if (!err) {
-                    console.log(`body:${body}`);
+                    console.log(body);
                     resolve("Done!")
                 } else {
                     console.error("Unable to send message:" + err);
