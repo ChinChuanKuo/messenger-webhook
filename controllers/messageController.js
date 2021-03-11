86c6fc8ecc6c96737803bc99a2ce2dc08a502864
ChinChuanKuo <chinchuankuo@ChinChuanKuos-MacBook-Pro.local>
2021-03-11 19:55:19 +0800

    module better

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -33,10 +33,10 @@ let handlePostMessage = async (req, res) => {
             // Gets the message. entry.messaging is an array, but 
             // will only ever contain one message, so we get index 0
             let webhook_event = entry.messaging[0];
-            //console.log(webhook_event);
+            console.log(webhook_event);
 
             let sender_psid = webhook_event.sender.id;
-            console.log(`Sender PSID: ${sender_psid}`);
+            //console.log(`Sender PSID: ${sender_psid}`);
 
             if (webhook_event.message) {
                 message.handleMessageAPI(sender_psid, webhook_event.message);
